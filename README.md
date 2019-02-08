b402607168e121e66d3eede1fc39c906fee58306
Kyoungsu Lee <yikster@gmail.com>
2019-02-08 10:42:30 +0900

    Added summary report. Cleansing codes.

--- README.md
+++ README.md
@@ -6,11 +6,48 @@
  
 # How to use this
 ## Prerequisites
-
-## Installing
+ * python3
 
 ## Running
+```bash
+[ec2-user@192.168.0.1 /home/ec2-user]$ git clone https://github.com/yikster/parse-ibatis
+[ec2-user@192.168.0.1 /home/ec2-user]$ cd parse-ibatis
+[ec2-user@192.168.0.1 /home/ec2-user]$ python3 parse-ibatis.py -p /home/ec2-user/data 
+```
+### result
+```bash
+Summary
+345 files are tested in /home/ec2-user/data
+1678 XML files are tested
+4000 SQL files are tested
+5678 queries are tested.
+100 queries has LIKE.
+10  unique table.columns are found.
+5 unique tabls are found.
+
+Unique Table Column List...
+	 0 TABLE_A.column_1
+	 1 TABLE_B.column_2
+	 2 TABLE_B.column_3
+	 3 TABLE_C.column_4
+	 4 TABLE_D.column_5
+	 5 TABLE_E.column_6
+	 6 TABLE_F.column_7
+	 7 TABLE_G.column_8
+	 8 TABLE_H.column_9
+	 9 TABLE_I.column_10
+	 
+	 
+All Query List using LIKE...
+     1. TABLE_A.column_1
+        1-01. file_sql_id sql
+        1-02. file_sql_id sql
+        1-03. file_sql_id sql
+	 
+```
 
 ## Versioning
+0.1 Find queries using LIKE in directory
 
 ## Authors
+Kyoungsu Lee, yikster@gmail.com
--- parse-ibatis.py
+++ parse-ibatis.py
@@ -1,15 +1,17 @@
 import argparse
-import xml.etree.ElementTree as xml
-import logging, sys
+import xml.etree.ElementTree as ElementTree
+import logging
+import sys
+
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 logging.debug('Debugging is started!')
 
-#logging.info('We processed %d records', len(processed_records))
 
-def readfiles(file_path, pattern):
+def get_files(file_path, pattern):
     import glob
 
     return glob.glob(file_path + "/**/" + pattern, recursive=True)
+
 
 def find_where_and_from(column, lines, index):
     i = index
@@ -19,7 +21,7 @@ def find_where_and_from(column, lines, index):
     while i > 0:
         if find_where == -1 and "WHERE" in lines[i]:
             find_where = i
-        if find_where != -1 and "FROM" in lines [i]:
+        if find_where != -1 and "FROM" in lines[i]:
             find_from = i
             break
         i = i - 1
@@ -42,7 +44,6 @@ def find_where_and_from(column, lines, index):
         froms = " ".join(from_where).split()
         logging.debug("Froms:" + str(froms))
 
-
         if "." in column:
             table_alias = column.split(".")[0]
 
@@ -52,18 +53,14 @@ def find_where_and_from(column, lines, index):
 
         else:
             table_name = froms[1]
-
-
         logging.debug("TableName: " + table_name)
+
     else:
         sys.exit()
 
-    sql = ""
-    if len(table_name) == 0:
-        sql = " ".join(lines)
+    sql = " ".join(lines).strip()
 
     return table_name, sql
-
 
 
 def extract_alias(column):
@@ -72,10 +69,28 @@ def extract_alias(column):
     else:
         return column
 
-def parseXML(file):
-    doc = None
+
+def parse(file):
+    if file.upper().endswith(".XML"):
+        return parse_xml(file)
+    elif file.upper().endswith(".SQL"):
+        return parse_sql(file)
+
+
+def parse_sql(file):
+    f = open(file)
+    lines = [line.rstrip('\n') for line in f]
+
+    result = parse_one_sql(lines, file, file, " ".join(lines).rstrip())
+    total_elements = 1  # sql file has only one sql
+    total_like_queries = len(result)
+    return total_elements, total_like_queries, result
+
+
+def parse_xml(file):
+
     try:
-        doc = xml.parse(file)
+        doc = ElementTree.parse(file)
     except Exception as ex:
         print("Errors in parsing [{}]".format(file))
         print(ex)
@@ -99,44 +114,40 @@ def parseXML(file):
     elements.extend(insert)
 
     total_elements = len(elements)
-    for e in elements: #root.findall(".//select"):
+    for e in elements:
         upper = e.text.upper()
         if "LIKE" in upper and '%' in upper:
             logging.debug(file)
             logging.debug(e.attrib['id'])
             logging.debug(e.text)
-            ## Like가 포함된 line ckwrl
+            sql_lines = upper.split("\n")
+            sql_id = e.attrib['id']
 
-            lines = e.text.split("\n")
-            for i, line in enumerate(lines):
-                if "LIKE" in line.upper():
-                    #print(i, line)
-                    words = line.upper().split("LIKE")
-                    column = words[0].split()[-1]
-                    logging.debug("Column:" +column)
-                    table, sql = find_where_and_from(column, lines, i)
-
-                    row = {"filename": file,
-                           "queryid": e.attrib['id'],
-                           "table": table,
-                           "column": extract_alias(column),
-                           "like_pharase": line,
-                           "sql": sql
-                    }
-                    result.append(row)
+            result.extend(parse_one_sql(sql_lines, sql_id, file, " ".join(e.text.split("\n")).rstrip()))
 
     return total_elements, len(result), result
 
-'''
-    for e in root.findall(".//sql"):
-        upper = e.text.upper()
-        if "LIKE" in upper and '%' in upper:
-            print(file)
-            print(e.attrib['id'])
-            print(e.text)
 
+def parse_one_sql(sql_lines, sql_id, file, raw_sql):
+    result = []
+    for i, line in enumerate(sql_lines):
+        if "LIKE" in line.upper():
 
-'''
+            words = line.upper().split("LIKE")
+            column = words[0].split()[-1]
+            logging.debug("Column:" + column)
+            table, sql = find_where_and_from(column, sql_lines, i)
+
+            row = {"filename": file,
+                   "query_id": sql_id,
+                   "table": table,
+                   "column": extract_alias(column),
+                   "likes": line,
+                   "sql": raw_sql
+                   }
+            result.append(row)
+    return result
+
 
 def main():
     parser = argparse.ArgumentParser(description='Parse iBatis xml files in path.')
@@ -144,41 +155,76 @@ def main():
 
     args = parser.parse_args()
 
-    files = readfiles(args.path, "*.xml")
+    files = get_files(args.path, "*.xml")
+    files.extend(get_files(args.path, "*.sql"))
 
-    all = []
     total_query_count = 0
     total_like_query_count = 0
+
+    xml_file_count = 0
+    sql_file_count = 0
+
+    all_data = []
+
     for file in files:
-    #print(file)
-        query_count, like_query_count, result = parseXML(file)
+        query_count, like_query_count, result = parse(file)
         total_query_count += query_count
         total_like_query_count += like_query_count
+        if file.upper().endswith(".XML"):
+            xml_file_count += 1
+        elif file.upper().endswith(".SQL"):
+            sql_file_count += 1
 
-        all.extend(result)
-
-#print(all)
+        all_data.extend(result)
 
     table_column = []
-    table_sqls = {}
-    for e in all:
+    sql_list = {}
+
+    unique_tables = {}
+    for e in all_data:
         key = e["table"].upper() + "." + e["column"].lower()
+        unique_tables[e["table"]] = e["table"]
         table_column.append(key)
-        table_sqls[key] = e["sql"]
+        if key not in sql_list.keys():
+            sql_list[key] = []
 
-    uniquelist = set(table_column)
-    unique_table_columns = len(uniquelist)
+        sql_file_id = e['filename'] + "..." + e['query_id']
+        sql_list[key].append({"key": key, "sql_file_id": sql_file_id, "sql": e["sql"]})
 
+    unique_table_column = set(table_column)
 
     print("Summary")
+    print("--------------------------------------------")
     print(len(files), "files are tested in", args.path)
+    print(xml_file_count, "XML files are tested.")
+    print(sql_file_count, "SQL files are tested.")
     print(total_query_count, "queries are tested.")
     print(total_like_query_count, "queries has LIKE.")
-    print("But, unique_table_columns is", unique_table_columns)
-    print("Unique Table Column List...")
-    for i, item in enumerate(sorted(uniquelist)):
-        print("\t",i, item, "!!! Check SQL!!! \n" + table_sqls[item] + "\n" if item.startswith(".") else " ")
+    print(len(unique_table_column), " unique table.columns are found.")
+    print(len(unique_tables), " unique tables are found.", )
 
+    print("\n\n\nUnique Table Column List...")
+    print("--------------------------------------------")
+    print("\tNo\tTABLE.column\tsql_counts\tfile_sql_id(first)\tsql(first)")
+    for i, key in enumerate(sorted(unique_table_column)):
+        print("\t%2d. %s\t%2d\t%s\t%s%s" %
+              (i + 1,
+               key,
+               len(sql_list[key]),
+               sql_list[key][0]["sql_file_id"],
+               "/* !!! Check SQL!!! */ " if key.startswith(".") else "",
+               sql_list[key][0]["sql"].strip().replace("  ", "")[:80]))
+
+    print("\n\n\nAll Query Lists using LIKE")
+    print("--------------------------------------------")
+    for i, key in enumerate(sorted(unique_table_column)):
+        print("%2d.\t%s" % (i + 1, key))
+        for j, data in enumerate(sql_list[key]):
+            print("\t%2d-%02d.\t%s\t%s" %
+                  (i + 1,
+                   j + 1,
+                   data["sql_file_id"],
+                   data["sql"].replace("  ", "").strip()))
 
 
 main()
